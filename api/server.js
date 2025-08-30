c508788305d466346d23a503f3f584e959004cd6
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-08-30 09:43:26 +0700

    fix bug

--- api/server.js
+++ api/server.js
@@ -4,8 +4,9 @@ import app from "../src/app.js";
 import { connectDb } from "../src/models/database.js";
 const port = 3000;
 
+connectDb();
+
 // menjalankan server express
 app.listen(port, async () => {
   console.log(`Server listening on port ${port}`);
-  await connectDb();
 });
